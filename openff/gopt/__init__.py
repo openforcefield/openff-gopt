64d07bd294d1027b31583daa97cfd115a5044e9c
David Dotson <dotsdl@gmail.com>
2021-04-29 21:07:14 -0700

    First commit, initial structure

---
+++ openff/gopt/__init__.py
@@ -0,0 +1,10 @@
+"""
+openff-gopt
+"""
+
+# Handle versioneer
+from ._version import get_versions
+versions = get_versions()
+__version__ = versions['version']
+__git_revision__ = versions['full-revisionid']
+del get_versions, versions
---
+++ setup.py
@@ -0,0 +1,77 @@
+"""
+OpenFF GOpt
+
+"""
+import os
+from os.path import relpath, join
+import sys
+from setuptools import setup, find_namespace_packages
+import versioneer
+
+short_description = __doc__.split("\n")
+
+# from https://github.com/pytest-dev/pytest-runner#conditional-requirement
+needs_pytest = {'pytest', 'test', 'ptr'}.intersection(sys.argv)
+pytest_runner = ['pytest-runner'] if needs_pytest else []
+
+try:
+    with open("README.md", "r") as handle:
+        long_description = handle.read()
+except:
+    long_description = "\n".join(short_description[2:])
+
+
+def find_package_data(data_root, package_root):
+    files = []
+    for root, dirnames, filenames in os.walk(data_root):
+        for fn in filenames:
+            files.append(relpath(join(root, fn), package_root))
+    return files
+
+
+setup(
+    # Self-descriptive entries which should always be present
+    name='openff-opt',
+    author='Open Forcefield Consortium',
+    author_email='jaime.rodriguez-guerra@openforcefield.org',
+    description=short_description[0],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
+    license='MIT',
+
+    entry_points={
+        "console_scripts": [
+            "openff-gopt = openff.gopt.cli:cli",
+        ]
+    },
+
+    package_data={'openff.gopt': find_package_data('openff/gopt/data', 'openff/gopt')},
+
+    # Which Python importable modules should be included when your package is installed
+    # Handled automatically by setuptools. Use 'exclude' to prevent some specific
+    # subpackage(s) from being added, if needed
+    packages=find_namespace_packages(include=['openff.*']),
+
+    # Optional include package data to ship with your package
+    # Customize MANIFEST.in if the general case does not suit your needs
+    # Comment out this line to prevent the files from being packaged with your software
+    include_package_data=True,
+
+    # Allows `setup.py test` to work correctly with pytest
+    setup_requires=[] + pytest_runner,
+
+    # Additional entries you may want simply uncomment the lines you want and fill in the data
+    # url='http://www.my_package.com',  # Website
+    # install_requires=[],              # Required packages, pulls from pip if needed; do not use for Conda deployment
+    # platforms=['Linux',
+    #            'Mac OS-X',
+    #            'Unix',
+    #            'Windows'],            # Valid platforms your code works on, adjust to your flavor
+    # python_requires=">=3.5",          # Python version restrictions
+
+    # Manual control if final package is compressible or not, set False to prevent the .egg from being made
+    # zip_safe=False,
+
+)
